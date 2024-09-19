76cd39847650e091728d07165b8372a70c8981c0
Carlos E. Peterson <71235444+EleazarPe@users.noreply.github.com>
2024-09-19 15:24:12 -0400

    feat(sponsors,codigoConducta): se agregaron los patrocinadores y codigo de conducta

---
+++ barcamp2024-astro/src/data/allEvents.ts
@@ -0,0 +1,53 @@
+const allEvents = [
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2024",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2023",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2022",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2021",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2020",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2019",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2018",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2017",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2016",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2015",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2014",
+    },
+    {
+        image: "/Icono-21.png",
+        description: "Barcamp 2013",
+    },
+
+];
+
+export { allEvents };
