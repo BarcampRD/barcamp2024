34265d880f34e348e8e984b58bbe4f1f416f29d6
Carlos E. Peterson <71235444+EleazarPe@users.noreply.github.com>
2024-09-19 15:59:05 -0400

    ya

--- barcamp2024-astro/src/data/allEvents.ts
+++ barcamp2024-astro/src/data/allEvents.ts
@@ -1,42 +1,38 @@
 const allEvents = [
     {
-        image: "/Icono-21.png",
-        description: "Barcamp 2024",
-    },
-    {
-        image: "/Icono-21.png",
+        image: "/OnePicTemp/2023/1.jpeg",
         description: "Barcamp 2023",
     },
     {
-        image: "/Icono-21.png",
+        image: "/OnePicTemp/2022/1.jpg",
         description: "Barcamp 2022",
     },
     {
-        image: "/Icono-21.png",
+        image: "/OnePicTemp/2021/1.jpg",
         description: "Barcamp 2021",
     },
     {
-        image: "/Icono-21.png",
+        image: "/OnePicTemp/2020/1.jpg",
         description: "Barcamp 2020",
     },
     {
-        image: "/Icono-21.png",
+        image: "/OnePicTemp/2019/1.jpg",
         description: "Barcamp 2019",
     },
     {
-        image: "/Icono-21.png",
+        image: "/OnePicTemp/2018/1.jpg",
         description: "Barcamp 2018",
     },
     {
-        image: "/Icono-21.png",
+        image: "/OnePicTemp/2017/1.JPG",
         description: "Barcamp 2017",
     },
     {
-        image: "/Icono-21.png",
+        image: "/OnePicTemp/2016/1.jpg",
         description: "Barcamp 2016",
     },
     {
-        image: "/Icono-21.png",
+        image: "/OnePicTemp/2015/1.jpg",
         description: "Barcamp 2015",
     },
     {
