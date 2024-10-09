8d91b71ba1c216ff3e2b1e9fb54b585608e961d3
IvanPaul1515 <ipmx0001@ce.pucmm.edu.do>
2024-10-09 15:03:14 -0400

    update error compil

--- src/data/allEvents.ts
+++ src/data/allEvents.ts
@@ -1,60 +1,184 @@
-const allEvents = [
+// const allEvents = [
+//     {
+//         image: "/OnePicTemp/2023/1.jpeg",
+//         description: "Barcamp 2023",
+//         link:"https://2023.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2022/1.jpg",
+//         description: "Barcamp 2022",
+//         link:"https://2022.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2021/1.jpg",
+//         description: "Barcamp 2021",
+//         link:"https://2021.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2020/1.jpg",
+//         description: "Barcamp 2020",
+//         link:"https://2020.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2019/1.jpg",
+//         description: "Barcamp 2019",
+//         link:"https://2019.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2018/1.jpg",
+//         description: "Barcamp 2018",
+//         link:"https://2018.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2017/1.JPG",
+//         description: "Barcamp 2017",
+//         link:"https://2017.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2016/1.jpg",
+//         description: "Barcamp 2016",
+//         link:"https://2016.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2015/1.jpg",
+//         description: "Barcamp 2015",
+//         link:"https://2015.barcamp.org.do/",
+//     },
+//     {
+//         image: "/Icono-21.png",
+//         description: "Barcamp 2014",
+//         link:"https://2014.barcamp.org.do/",
+//     },
+//     {
+//         image: "/Icono-21.png",
+//         description: "Barcamp 2013",
+//         link:"https://2013.barcamp.org.do/",
+//     },
+//
+// ];
+//
+// export { allEvents };
+
+// src/data/allEvents.ts
+// const allEvents = [
+//     {
+//         image: "/OnePicTemp/2023/1.jpeg",
+//         description: "Barcamp 2023",
+//         link:"https://2023.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2022/1.jpg",
+//         description: "Barcamp 2022",
+//         link:"https://2022.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2021/1.jpg",
+//         description: "Barcamp 2021",
+//         link:"https://2021.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2020/1.jpg",
+//         description: "Barcamp 2020",
+//         link:"https://2020.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2019/1.jpg",
+//         description: "Barcamp 2019",
+//         link:"https://2019.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2018/1.jpg",
+//         description: "Barcamp 2018",
+//         link:"https://2018.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2017/1.JPG",
+//         description: "Barcamp 2017",
+//         link:"https://2017.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2016/1.jpg",
+//         description: "Barcamp 2016",
+//         link:"https://2016.barcamp.org.do/",
+//     },
+//     {
+//         image: "/OnePicTemp/2015/1.jpg",
+//         description: "Barcamp 2015",
+//         link:"https://2015.barcamp.org.do/",
+//     },
+//     {
+//         image: "/Icono-21.png",
+//         description: "Barcamp 2014",
+//         link:"https://2014.barcamp.org.do/",
+//     },
+//     {
+//         image: "/Icono-21.png",
+//         description: "Barcamp 2013",
+//         link:"https://2013.barcamp.org.do/",
+//     },
+//
+// ];
+//
+// export { allEvents };
+// src/data/allEvents.ts
+import type {TestimonialItem} from "../types/TestimonialItem";
+
+const allEvents: TestimonialItem[] = [
     {
         image: "/OnePicTemp/2023/1.jpeg",
         description: "Barcamp 2023",
-        link:"https://2023.barcamp.org.do/",
+        link: "https://2023.barcamp.org.do/",
     },
     {
         image: "/OnePicTemp/2022/1.jpg",
         description: "Barcamp 2022",
-        link:"https://2022.barcamp.org.do/",
+        link: "https://2022.barcamp.org.do/",
     },
     {
         image: "/OnePicTemp/2021/1.jpg",
         description: "Barcamp 2021",
-        link:"https://2021.barcamp.org.do/",
+        link: "https://2021.barcamp.org.do/",
     },
     {
         image: "/OnePicTemp/2020/1.jpg",
         description: "Barcamp 2020",
-        link:"https://2020.barcamp.org.do/",
+        link: "https://2020.barcamp.org.do/",
     },
     {
         image: "/OnePicTemp/2019/1.jpg",
         description: "Barcamp 2019",
-        link:"https://2019.barcamp.org.do/",
+        link: "https://2019.barcamp.org.do/",
     },
     {
         image: "/OnePicTemp/2018/1.jpg",
         description: "Barcamp 2018",
-        link:"https://2018.barcamp.org.do/",
+        link: "https://2018.barcamp.org.do/",
     },
     {
         image: "/OnePicTemp/2017/1.JPG",
         description: "Barcamp 2017",
-        link:"https://2017.barcamp.org.do/",
+        link: "https://2017.barcamp.org.do/",
     },
     {
         image: "/OnePicTemp/2016/1.jpg",
         description: "Barcamp 2016",
-        link:"https://2016.barcamp.org.do/",
+        link: "https://2016.barcamp.org.do/",
     },
     {
         image: "/OnePicTemp/2015/1.jpg",
         description: "Barcamp 2015",
-        link:"https://2015.barcamp.org.do/",
+        link: "https://2015.barcamp.org.do/",
     },
     {
         image: "/Icono-21.png",
         description: "Barcamp 2014",
-        link:"https://2014.barcamp.org.do/",
+        link: "https://2014.barcamp.org.do/",
     },
     {
         image: "/Icono-21.png",
         description: "Barcamp 2013",
-        link:"https://2013.barcamp.org.do/",
+        link: "https://2013.barcamp.org.do/",
     },
-
 ];
 
 export { allEvents };
---
+++ src/types/TestimonialItem.ts
@@ -0,0 +1,6 @@
+
+export interface TestimonialItem {
+    image: string;
+    description: string;
+    link: string;
+}
