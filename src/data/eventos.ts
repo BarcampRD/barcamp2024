95c90a0191947ab3975cdf476f129e9b7ae18a17
IvanPaul1515 <ipmx0001@ce.pucmm.edu.do>
2024-10-14 18:25:17 -0400

    update, speakers y programa listo

---
+++ src/data/eventos.ts
@@ -0,0 +1,28 @@
+
+const eventos = [
+    {
+        horario: "8:00 - 9:00",
+        actividad: "Registro y Bienvenida al Evento",
+        ponente: "Carlos Camacho",
+        detalles: "Bienvenida al Evento Barcamp RD 2024",
+        etiquetas: ["Barcamp RD", "2022", "12 años"],
+        lugar: "Auditorio de la Salud",
+    },
+    {
+        horario: "9:00 - 9:30",
+        actividad: "Análisis de Nubes Privadas en PyMES",
+        ponente: "Antonio Perpiñan",
+        detalles: "Desbloqueando el potencial de la nube privada en el mundo empresarial",
+        etiquetas: ["Nube", "Linux", "Software Libre"],
+        lugar: "Auditorio de la Salud",
+    },
+    {
+        horario: "9:30 - 10:00",
+        actividad: "Integración de Técnicas de Aprendizaje de Máquina",
+        ponente: "Rafael Brito",
+        detalles: "Aplicación de machine learning en sistemas eléctricos",
+        etiquetas: ["Potencia", "IA"],
+        lugar: "Sala de Conferencias A",
+    },
+];
+export { eventos };
---
+++ src/data/speakers.ts
@@ -0,0 +1,26 @@
+export const speakers = [
+    {
+        name: "Darian Vargas",
+        title: "Ingeniero Telemático, fundador de AvatharTech y director...",
+        role: "Charla Magistral",
+        imageUrl: "/speakers/darian.jpg",
+    },
+    {
+        name: "Antonio Perpiñan",
+        title: "Matemático",
+        role: "Ponente",
+        imageUrl: "/speakers/darian.jpg",
+    },
+    {
+        name: "Rafael Batista",
+        title: "Ingeniero de Electrónica",
+        role: "Ponente",
+        imageUrl: "/speakers/darian.jpg",
+    },
+    {
+        name: "Felix Valerio",
+        title: "Ingeniero de Software",
+        role: "Ponente",
+        imageUrl: "/speakers/darian.jpg",
+    },
+];
