fa189782da8d729ccd96db67b59268b547a21a35
Carlos E. Peterson <71235444+EleazarPe@users.noreply.github.com>
2024-10-19 17:05:43 -0400

    feat(empresas, invitado y ubicacion): se agregaron empresa patrocinadores, invitado especial y la ubicacion Padre Arroyo

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -1,26 +1,26 @@
 export const speakers = [
-    {
-        name: "Darian Vargas",
-        title: "Ingeniero Telemático, fundador de AvatharTech y director...",
-        role: "Charla Magistral",
-        imageUrl: "/speakers/darian.jpg",
-    },
-    {
-        name: "Antonio Perpiñan",
-        title: "Matemático",
-        role: "Ponente",
-        imageUrl: "/speakers/darian.jpg",
-    },
-    {
-        name: "Rafael Batista",
-        title: "Ingeniero de Electrónica",
-        role: "Ponente",
-        imageUrl: "/speakers/darian.jpg",
-    },
-    {
-        name: "Felix Valerio",
-        title: "Ingeniero de Software",
-        role: "Ponente",
-        imageUrl: "/speakers/darian.jpg",
-    },
+  {
+    name: "Darian Vargas",
+    title: "Ingeniero Telemático, fundador de AvatharTech y director...",
+    role: "Charla Magistral",
+    imageUrl: "/speakers/darianpng.png",
+  },
+  {
+    name: "Antonio Perpiñan",
+    title: "Matemático",
+    role: "Ponente",
+    imageUrl: "/speakers/darian.jpg",
+  },
+  {
+    name: "Rafael Batista",
+    title: "Ingeniero de Electrónica",
+    role: "Ponente",
+    imageUrl: "/speakers/darian.jpg",
+  },
+  {
+    name: "Felix Valerio",
+    title: "Ingeniero de Software",
+    role: "Ponente",
+    imageUrl: "/speakers/darian.jpg",
+  },
 ];
