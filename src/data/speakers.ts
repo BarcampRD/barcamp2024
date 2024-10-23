8ba3e132730b4ed9268c8caf20efa68c2a84374c
IvanPaul1515 <ipmx0001@ce.pucmm.edu.do>
2024-10-23 16:15:27 -0400

    speakers

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -1,26 +1,38 @@
+
 export const speakers = [
   {
-    name: "Darian Vargas",
-    title: "Ingeniero Telemático, fundador de AvatharTech y director...",
-    role: "Charla Magistral",
-    imageUrl: "/speakers/darianpng.png",
+    name: "Joas Yaminaki",
+    title: "Backend Developer",
+    charla: "Golang in Practice: Boosting Efficiency with Go Routines ",
+    imageUrl: "/public/speakers/Joas Yaminaki.png",
+    borderColor: "#00ACD7",  // Color de borde personalizado
+    details: "Desarrollador Backend Especializado en Go (Golang) | Creación de soluciones escalables y de alto rendimiento"
   },
+  // {
+  //   name: "Darvy Betances",
+  //   title: "Computer Science Engineer",
+  //   charla: "Innovación y Futuro: Retos Globales, IA, y el Poder de la Creatividad ",
+  //   imageUrl: "/public/speakers/darvy betances.png",
+  //   borderColor: "#E3FF00",  // Color de borde personalizado
+  //   details: "Ingeniero en Ciencias de la Computación con enfoque en desarrollo backend. Me apasiona el ecosistema Java y los campos de la ingeniería y ciencia de datos. Siempre estoy buscando aprender y mejorar mis habilidades, disfrutando el proceso de enfrentar nuevos desafíos en este dominio tecnológico."
+  // },
   {
-    name: "Antonio Perpiñan",
-    title: "Matemático",
-    role: "Ponente",
-    imageUrl: "/speakers/darian.jpg",
+    name: "Jonathan Morales",
+    title: "Technical Advisor & Configuration Manager",
+    charla: " Del Código a la Conexión Emocional: El Rol de la Experiencia (XM) en Proyectos Tecnológicos",
+    imageUrl: "public/speakers/Jonathan Morales .png",
+    borderColor: "#E6671D",  // Color de borde personalizado
+    details: "Ingeniero en Sistemas y Máster en Alta Gerencia con mención en Finanzas por el Instituto Tecnológico de Santo Domingo (INTEC)." +
+        "Con más de 15 años de experiencia, he trabajado en la adopción de marcos de buenas prácticas globales, tales como ITIL, ISO/IEC 20000, ISO/IEC 27001, COBIT, y PMBOK, orientados a lograr una eficiencia operativa alineada con el negocio en el ámbito de TI. Mi enfoque se centra en la integración de personas, procesos y tecnología para generar mejoras significativas y sostenibles.\n" +
+        "Apasionado por compartir conocimiento y aprender, disfruto de optimizar procesos y explorar nuevas formas de hacer las cosas. Me motiva ver cómo una organización puede evolucionar al abordar sus desafíos de manera integral, y me comprometo a ayudar a las empresas a mejorar su manera de operar, innovar y atender a sus clientes y usuarios. Soy miembro activo de la comunidad ITSM RD, donde colaboro para impulsar la gestión de servicios de TI en la región.\n" +
+        "Estoy enganchado a ayudar a las organizaciones a mejorar su forma de trabajar, innovar y atender a sus clientes  usuarios por eso colaboro como parte de la comunidad de ITSM RD."
   },
-  {
-    name: "Rafael Batista",
-    title: "Ingeniero de Electrónica",
-    role: "Ponente",
-    imageUrl: "/speakers/darian.jpg",
-  },
-  {
-    name: "Felix Valerio",
-    title: "Ingeniero de Software",
-    role: "Ponente",
-    imageUrl: "/speakers/darian.jpg",
-  },
+  // {
+  //   name: "Felix Valerio",
+  //   title: "Software Developer",
+  //   charla: "The Upgrade Journey: Real-World Stories of Symfony Version Migrations",
+  //   imageUrl: "/public/speakers/Felix Valerio.png",
+  //   borderColor: "#4CAF50",  // Color de borde personalizado
+  //   details: "Desarrollador Backend Especializado en Go (Golang) | Creación de soluciones escalables y de alto rendimiento"
+  // },
 ];
