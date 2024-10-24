6054bdf2fb4b10212a1aee4a92b83443448b91dc
Carlos E. Peterson <71235444+EleazarPe@users.noreply.github.com>
2024-10-24 15:19:58 -0400

    fix(reset to 8ba3e132730b4ed9268c8caf20efa68c2a84374c)

--- src/data/allEvents.ts
+++ src/data/allEvents.ts
@@ -1,52 +1,96 @@
-
-import type {TestimonialItem} from "../types/TestimonialItem";
+import type { TestimonialItem } from "../types/TestimonialItem";
 
 const allEvents: TestimonialItem[] = [
-    {
-        image: ["/events/2023/DSC_0009.JPG", "/events/2023/DSC_0012.JPG", "/events/2023/DSC_0095.JPG", "/events/2023/DSC_0151.JPG"],
-        description: "Barcamp 2023 modificada",
-        link: "https://2023.barcamp.org.do/",
-    },
-    {
-        image: ["/events/2022/20221126_095815_small.jpg", "/events/2022/20221126_105246_small.jpg", "/events/2022/20221126_120313_small.jpg", "/events/2022/IMG_20221126_150620_small.jpg"],
-        description: "Barcamp 2022",
-        link: "https://2022.barcamp.org.do/",
-    },
-    {
-        image: ["/events/2021/1.jpg", "/events/2021/2.jpg", "/events/2021/3.jpg", "/events/2021/4.jpg"],
-        description: "Barcamp 2021",
-        link: "https://2021.barcamp.org.do/",
-    },
-    {
-        image: ["/events/2020/1.jpg", "/events/2020/2.jpg", "/events/2020/1.jpg", "/events/2020/2.jpg"],
-        description: "Barcamp 2020",
-        link: "https://2020.barcamp.org.do/",
-    },
-    {
-        image: ["/events/2019/1.jpg", "/events/2019/2.jpg", "/events/2019/3.jpg", "/events/2019/4.jpg"],
-        description: "Barcamp 2019",
-        link: "https://2019.barcamp.org.do/",
-    },
-    {
-        image: ["/events/2018/1.jpg", "/events/2018/2.jpg", "/events/2018/3.jpg", "/events/2018/4.jpg"],
-        description: "Barcamp 2018",
-        link: "https://2018.barcamp.org.do/",
-    },
-    {
-        image: ["/events/2017/1.jpg", "/events/2017/2.JPG", "/events/2017/3.JPG", "/events/2017/4.JPG"],
-        description: "Barcamp 2017",
-        link: "https://2017.barcamp.org.do/",
-    },
-    {
-        image: ["/events/2016/1.jpg", "/events/2016/2.jpg", "/events/2016/3.jpg", "/events/2016/4.jpg"],
-        description: "Barcamp 2016",
-        link: "https://2016.barcamp.org.do/",
-    },
-    {
-        image: ["/events/2015/1.jpg", "/events/2015/2.jpeg", "/events/2015/3.jpeg", "/events/2015/4.jpg"],
-        description: "Barcamp 2015",
-        link: "https://2015.barcamp.org.do/",
-    },
+  {
+    image: [
+      "/events/2023/DSC_0009.JPG",
+      "/events/2023/DSC_0012.JPG",
+      "/events/2023/DSC_0095.JPG",
+      "/events/2023/DSC_0151.JPG",
+    ],
+    description: "Barcamp 2023",
+    link: "https://2023.barcamp.org.do/",
+  },
+  {
+    image: [
+      "/events/2022/20221126_095815_small.jpg",
+      "/events/2022/20221126_105246_small.jpg",
+      "/events/2022/20221126_120313_small.jpg",
+      "/events/2022/IMG_20221126_150620_small.jpg",
+    ],
+    description: "Barcamp 2022",
+    link: "https://2022.barcamp.org.do/",
+  },
+  {
+    image: [
+      "/events/2021/1.jpg",
+      "/events/2021/2.jpg",
+      "/events/2021/3.jpg",
+      "/events/2021/4.jpg",
+    ],
+    description: "Barcamp 2021",
+    link: "https://2021.barcamp.org.do/",
+  },
+  {
+    image: [
+      "/events/2020/1.jpg",
+      "/events/2020/2.jpg",
+      "/events/2020/1.jpg",
+      "/events/2020/2.jpg",
+    ],
+    description: "Barcamp 2020",
+    link: "https://2020.barcamp.org.do/",
+  },
+  {
+    image: [
+      "/events/2019/1.jpg",
+      "/events/2019/2.jpg",
+      "/events/2019/3.jpg",
+      "/events/2019/4.jpg",
+    ],
+    description: "Barcamp 2019",
+    link: "https://2019.barcamp.org.do/",
+  },
+  {
+    image: [
+      "/events/2018/1.jpg",
+      "/events/2018/2.jpg",
+      "/events/2018/3.jpg",
+      "/events/2018/4.jpg",
+    ],
+    description: "Barcamp 2018",
+    link: "https://2018.barcamp.org.do/",
+  },
+  {
+    image: [
+      "/events/2017/1.jpg",
+      "/events/2017/2.JPG",
+      "/events/2017/3.JPG",
+      "/events/2017/4.JPG",
+    ],
+    description: "Barcamp 2017",
+    link: "https://2017.barcamp.org.do/",
+  },
+  {
+    image: [
+      "/events/2016/1.jpg",
+      "/events/2016/2.jpg",
+      "/events/2016/3.jpg",
+      "/events/2016/4.jpg",
+    ],
+    description: "Barcamp 2016",
+    link: "https://2016.barcamp.org.do/",
+  },
+  {
+    image: [
+      "/events/2015/1.jpg",
+      "/events/2015/2.jpeg",
+      "/events/2015/3.jpeg",
+      "/events/2015/4.jpg",
+    ],
+    description: "Barcamp 2015",
+    link: "https://2015.barcamp.org.do/",
+  },
 ];
 
 export { allEvents };
