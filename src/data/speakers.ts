c4b8f92ae959aef0277e91cb76d4e74f3b7d7ef9
IvanPaul1515 <ipmx0001@ce.pucmm.edu.do>
2024-10-25 00:36:02 -0400

    fix3

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -8,14 +8,6 @@ export const speakers = [
     borderColor: "#00ACD7",  // Color de borde personalizado
     details: "Desarrollador Backend Especializado en Go (Golang) | Creación de soluciones escalables y de alto rendimiento"
   },
-  // {
-  //   name: "Darvy Betances",
-  //   title: "Computer Science Engineer",
-  //   charla: "Innovación y Futuro: Retos Globales, IA, y el Poder de la Creatividad ",
-  //   imageUrl: "/public/speakers/darvy betances.png",
-  //   borderColor: "#E3FF00",  // Color de borde personalizado
-  //   details: "Ingeniero en Ciencias de la Computación con enfoque en desarrollo backend. Me apasiona el ecosistema Java y los campos de la ingeniería y ciencia de datos. Siempre estoy buscando aprender y mejorar mis habilidades, disfrutando el proceso de enfrentar nuevos desafíos en este dominio tecnológico."
-  // },
   {
     name: "Jonathan Morales",
     title: "Technical Advisor & Configuration Manager",
@@ -27,12 +19,63 @@ export const speakers = [
         "Apasionado por compartir conocimiento y aprender, disfruto de optimizar procesos y explorar nuevas formas de hacer las cosas. Me motiva ver cómo una organización puede evolucionar al abordar sus desafíos de manera integral, y me comprometo a ayudar a las empresas a mejorar su manera de operar, innovar y atender a sus clientes y usuarios. Soy miembro activo de la comunidad ITSM RD, donde colaboro para impulsar la gestión de servicios de TI en la región.\n" +
         "Estoy enganchado a ayudar a las organizaciones a mejorar su forma de trabajar, innovar y atender a sus clientes  usuarios por eso colaboro como parte de la comunidad de ITSM RD."
   },
-  // {
-  //   name: "Felix Valerio",
-  //   title: "Software Developer",
-  //   charla: "The Upgrade Journey: Real-World Stories of Symfony Version Migrations",
-  //   imageUrl: "/public/speakers/Felix Valerio.png",
-  //   borderColor: "#4CAF50",  // Color de borde personalizado
-  //   details: "Desarrollador Backend Especializado en Go (Golang) | Creación de soluciones escalables y de alto rendimiento"
-  // },
+  {
+    name: "Juan Polanco",
+    title: "Software Developer",
+    charla: "Desplegando Deep Learning a producción con ONNX",
+    imageUrl: "/public/speakers/Juan polanco.png",
+    borderColor: "#72539A",  // Color de borde personalizado
+    details: "Soy un ingeniero telemático y desarrollador de software al que le gusta crear software relacionado especialmente con la web, el aprendizaje automático y la inteligencia artificial. Entre los proyectos que tengo como parte de mis estudios de Machine Learning, hay un modelo que utiliza datos del mercado de bonos de Estados Unidos que me permite estimar el precio de un bono en función de sus últimas 10 transacciones. Quiero combinar mi experiencia como desarrollador de software completo y mi conocimiento de aprendizaje automático para crear productos que resuelvan problemas del mundo real."
+  },
+  {
+    name: "Jose Martinez",
+    title: "Software Engineer",
+    charla: "Ingeniería de Datos con Apache Airflow",
+    imageUrl: "/public/speakers/jose martinez.png",
+    borderColor: "#00AD46",  // Color de borde personalizado
+    details: "Ingeniero de software, con experiencia en desarrollo de software full stack, gestión de proyectos y proyectos de aprendizaje automático. He trabajado en el desarrollo de productos innovadores para clientes en colaboración con múltiples equipos de software y múltiples pilas de tecnología, así como en la mejora y el desarrollo de herramientas empresariales internas para impulsar la productividad y la eficiencia de los procesos.\n" +
+        "\n" +
+        "Tengo experiencia con una amplia variedad de tecnologías, principalmente Java, Spring Boot, Angular y Docker, así como R Language y MongoDB para proyectos ML y Python para Procesamiento de Datos, ETL's y Data Pipelining. También he utilizado el marco Scrum como Product Owner, para liderar múltiples equipos y lograr el rol comercial.\n" +
+        "\n" +
+        "Me considero un profesional honesto, responsable y organizado, que prospera resolviendo problemas relacionados con los negocios con tecnología, tiene habilidades de liderazgo y se enorgullece de hacer el mejor trabajo posible."
+  },
+  {
+    name: "Ruth Castillo",
+    title: "IT Consultant",
+    charla: "Más allá de Scrum: Ágil Disciplinado",
+    imageUrl: "/public/speakers/ruth castillo.png",
+    borderColor: "#F06A6B",  // Color de borde personalizado
+    details: ""
+  },
+  {
+    name: "Yhorby Matias",
+    title: "DevOps Engineer",
+    charla: "Crea Entornos de Prueba Automáticos con Azure y Argo CD",
+    imageUrl: "/public/speakers/yhorby matias.png",
+    borderColor: "#FE733E",  // Color de borde personalizado
+    details: "Profesional con 9 años de experiencia con Licenciatura en Ciencias de la Computación (BSCS). Comencé mi carrera en TI \n" +
+        "como desarrollador back-end y trabajé durante varios años en este rol, luego hice el cambio al rol de DevOps, \n" +
+        "habiendo trabajado con aplicaciones basadas en Azure/AWS/GCP durante cinco años, he gestionado migraciones, \n" +
+        "flujos de trabajo optimizados y colaboración con equipos de ingeniería para garantizar el máximo impacto empresarial, trabajando \n" +
+        "estrechamente con los miembros de mi equipo de DevOps para desarrollar las mejores soluciones escalables de su clase. Logramos reducir \n" +
+        "el tiempo de implementación de la infraestructura en un 150 por ciento, diseñando canales de CI/CD eficientes.\n" +
+        "En términos de mi conocimiento de herramientas y soluciones, soy competente en Datadog, Kubernetes, Azure, AWS y \n" +
+        "Nubes privadas junto con cinco lenguajes de programación principales."
+  },
+  {
+    name: "Juan Gonzalez",
+    title: "Ingeniero Electrico",
+    charla: "Innovación, el motor del desarrollo tecnológico",
+    imageUrl: "/public/speakers/juan gonzalez.png",
+    borderColor: "#884E85",  // Color de borde personalizado
+    details: "Más de 15 años de experiencia en instalaciones eléctricas, diseño de sistemas de iluminación, asesor energético y consultor de transformación digital.\n"
+  },
+  {
+    name: "Karla Aquino",
+    title: "IT Consultant",
+    charla: "Beyond Code: Integrated Security with DevSecOps and SSDLC",
+    imageUrl: "/public/speakers/karla aquino.png",
+    borderColor: "#026691",  // Color de borde personalizado
+    details: ""
+  },
 ];
