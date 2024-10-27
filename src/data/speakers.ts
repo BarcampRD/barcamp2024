05e9450572c80c9c4a4300ecb3e5d1079676b714
Carlos E. Peterson <71235444+EleazarPe@users.noreply.github.com>
2024-10-26 20:53:06 -0400

    feat(modal): avances en los modal de los card

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -1,4 +1,3 @@
-
 export const speakers = [
   {
     id: 1,
@@ -6,20 +5,23 @@ export const speakers = [
     title: "Backend Developer",
     charla: "Golang in Practice: Boosting Efficiency with Go Routines ",
     imageUrl: "public/speakers img/Joas Yaminaki.JPG",
-    borderColor: "#00ACD7",  // Color de borde personalizado
-    details: "Desarrollador Backend Especializado en Go (Golang) | Creación de soluciones escalables y de alto rendimiento"
+    borderColor: "#00cbfe", // Color de borde personalizado
+    details:
+      "Desarrollador Backend Especializado en Go (Golang) | Creación de soluciones escalables y de alto rendimiento",
   },
   {
     id: 2,
     name: "Jonathan Morales",
     title: "Technical Advisor & Configuration Manager",
-    charla: " Del Código a la Conexión Emocional: El Rol de la Experiencia (XM) en Proyectos Tecnológicos",
+    charla:
+      " Del Código a la Conexión Emocional: El Rol de la Experiencia (XM) en Proyectos Tecnológicos",
     imageUrl: "public/speakers img/Jonathan Morales.png",
-    borderColor: "#E6671D",  // Color de borde personalizado
-    details: "Ingeniero en Sistemas y Máster en Alta Gerencia con mención en Finanzas por el Instituto Tecnológico de Santo Domingo (INTEC)." +
-        "Con más de 15 años de experiencia, he trabajado en la adopción de marcos de buenas prácticas globales, tales como ITIL, ISO/IEC 20000, ISO/IEC 27001, COBIT, y PMBOK, orientados a lograr una eficiencia operativa alineada con el negocio en el ámbito de TI. Mi enfoque se centra en la integración de personas, procesos y tecnología para generar mejoras significativas y sostenibles.\n" +
-        "Apasionado por compartir conocimiento y aprender, disfruto de optimizar procesos y explorar nuevas formas de hacer las cosas. Me motiva ver cómo una organización puede evolucionar al abordar sus desafíos de manera integral, y me comprometo a ayudar a las empresas a mejorar su manera de operar, innovar y atender a sus clientes y usuarios. Soy miembro activo de la comunidad ITSM RD, donde colaboro para impulsar la gestión de servicios de TI en la región.\n" +
-        "Estoy enganchado a ayudar a las organizaciones a mejorar su forma de trabajar, innovar y atender a sus clientes  usuarios por eso colaboro como parte de la comunidad de ITSM RD."
+    borderColor: "#00cbfe", // Color de borde personalizado
+    details:
+      "Ingeniero en Sistemas y Máster en Alta Gerencia con mención en Finanzas por el Instituto Tecnológico de Santo Domingo (INTEC)." +
+      "Con más de 15 años de experiencia, he trabajado en la adopción de marcos de buenas prácticas globales, tales como ITIL, ISO/IEC 20000, ISO/IEC 27001, COBIT, y PMBOK, orientados a lograr una eficiencia operativa alineada con el negocio en el ámbito de TI. Mi enfoque se centra en la integración de personas, procesos y tecnología para generar mejoras significativas y sostenibles.\n" +
+      "Apasionado por compartir conocimiento y aprender, disfruto de optimizar procesos y explorar nuevas formas de hacer las cosas. Me motiva ver cómo una organización puede evolucionar al abordar sus desafíos de manera integral, y me comprometo a ayudar a las empresas a mejorar su manera de operar, innovar y atender a sus clientes y usuarios. Soy miembro activo de la comunidad ITSM RD, donde colaboro para impulsar la gestión de servicios de TI en la región.\n" +
+      "Estoy enganchado a ayudar a las organizaciones a mejorar su forma de trabajar, innovar y atender a sus clientes  usuarios por eso colaboro como parte de la comunidad de ITSM RD.",
   },
   {
     id: 3,
@@ -27,8 +29,9 @@ export const speakers = [
     title: "Software Developer",
     charla: "Desplegando Deep Learning a producción con ONNX",
     imageUrl: "public/speakers img/Juan Polanco.jpeg",
-    borderColor: "#72539A",  // Color de borde personalizado
-    details: "Soy un ingeniero telemático y desarrollador de software al que le gusta crear software relacionado especialmente con la web, el aprendizaje automático y la inteligencia artificial. Entre los proyectos que tengo como parte de mis estudios de Machine Learning, hay un modelo que utiliza datos del mercado de bonos de Estados Unidos que me permite estimar el precio de un bono en función de sus últimas 10 transacciones. Quiero combinar mi experiencia como desarrollador de software completo y mi conocimiento de aprendizaje automático para crear productos que resuelvan problemas del mundo real."
+    borderColor: "#00cbfe", // Color de borde personalizado
+    details:
+      "Soy un ingeniero telemático y desarrollador de software al que le gusta crear software relacionado especialmente con la web, el aprendizaje automático y la inteligencia artificial. Entre los proyectos que tengo como parte de mis estudios de Machine Learning, hay un modelo que utiliza datos del mercado de bonos de Estados Unidos que me permite estimar el precio de un bono en función de sus últimas 10 transacciones. Quiero combinar mi experiencia como desarrollador de software completo y mi conocimiento de aprendizaje automático para crear productos que resuelvan problemas del mundo real.",
   },
   {
     id: 4,
@@ -36,12 +39,13 @@ export const speakers = [
     title: "Software Engineer",
     charla: "Ingeniería de Datos con Apache Airflow",
     imageUrl: "public/speakers img/Jose Manuel Martínez.jpeg",
-    borderColor: "#00AD46",  // Color de borde personalizado
-    details: "Ingeniero de software, con experiencia en desarrollo de software full stack, gestión de proyectos y proyectos de aprendizaje automático. He trabajado en el desarrollo de productos innovadores para clientes en colaboración con múltiples equipos de software y múltiples pilas de tecnología, así como en la mejora y el desarrollo de herramientas empresariales internas para impulsar la productividad y la eficiencia de los procesos.\n" +
-        "\n" +
-        "Tengo experiencia con una amplia variedad de tecnologías, principalmente Java, Spring Boot, Angular y Docker, así como R Language y MongoDB para proyectos ML y Python para Procesamiento de Datos, ETL's y Data Pipelining. También he utilizado el marco Scrum como Product Owner, para liderar múltiples equipos y lograr el rol comercial.\n" +
-        "\n" +
-        "Me considero un profesional honesto, responsable y organizado, que prospera resolviendo problemas relacionados con los negocios con tecnología, tiene habilidades de liderazgo y se enorgullece de hacer el mejor trabajo posible."
+    borderColor: "#00cbfe", // Color de borde personalizado
+    details:
+      "Ingeniero de software, con experiencia en desarrollo de software full stack, gestión de proyectos y proyectos de aprendizaje automático. He trabajado en el desarrollo de productos innovadores para clientes en colaboración con múltiples equipos de software y múltiples pilas de tecnología, así como en la mejora y el desarrollo de herramientas empresariales internas para impulsar la productividad y la eficiencia de los procesos.\n" +
+      "\n" +
+      "Tengo experiencia con una amplia variedad de tecnologías, principalmente Java, Spring Boot, Angular y Docker, así como R Language y MongoDB para proyectos ML y Python para Procesamiento de Datos, ETL's y Data Pipelining. También he utilizado el marco Scrum como Product Owner, para liderar múltiples equipos y lograr el rol comercial.\n" +
+      "\n" +
+      "Me considero un profesional honesto, responsable y organizado, que prospera resolviendo problemas relacionados con los negocios con tecnología, tiene habilidades de liderazgo y se enorgullece de hacer el mejor trabajo posible.",
   },
   {
     id: 5,
@@ -49,8 +53,8 @@ export const speakers = [
     title: "IT Consultant",
     charla: "Más allá de Scrum: Ágil Disciplinado",
     imageUrl: "public/speakers img/Ruth Castillo.jpeg",
-    borderColor: "#F06A6B",  // Color de borde personalizado
-    details: ""
+    borderColor: "#00cbfe", // Color de borde personalizado
+    details: "",
   },
   {
     id: 6,
@@ -58,15 +62,16 @@ export const speakers = [
     title: "DevOps Engineer",
     charla: "Crea Entornos de Prueba Automáticos con Azure y Argo CD",
     imageUrl: "public/speakers img/Yhorby Matias.jpeg",
-    borderColor: "#FE733E",  // Color de borde personalizado
-    details: "Profesional con 9 años de experiencia con Licenciatura en Ciencias de la Computación (BSCS). Comencé mi carrera en TI \n" +
-        "como desarrollador back-end y trabajé durante varios años en este rol, luego hice el cambio al rol de DevOps, \n" +
-        "habiendo trabajado con aplicaciones basadas en Azure/AWS/GCP durante cinco años, he gestionado migraciones, \n" +
-        "flujos de trabajo optimizados y colaboración con equipos de ingeniería para garantizar el máximo impacto empresarial, trabajando \n" +
-        "estrechamente con los miembros de mi equipo de DevOps para desarrollar las mejores soluciones escalables de su clase. Logramos reducir \n" +
-        "el tiempo de implementación de la infraestructura en un 150 por ciento, diseñando canales de CI/CD eficientes.\n" +
-        "En términos de mi conocimiento de herramientas y soluciones, soy competente en Datadog, Kubernetes, Azure, AWS y \n" +
-        "Nubes privadas junto con cinco lenguajes de programación principales."
+    borderColor: "#00cbfe", // Color de borde personalizado
+    details:
+      "Profesional con 9 años de experiencia con Licenciatura en Ciencias de la Computación (BSCS). Comencé mi carrera en TI \n" +
+      "como desarrollador back-end y trabajé durante varios años en este rol, luego hice el cambio al rol de DevOps, \n" +
+      "habiendo trabajado con aplicaciones basadas en Azure/AWS/GCP durante cinco años, he gestionado migraciones, \n" +
+      "flujos de trabajo optimizados y colaboración con equipos de ingeniería para garantizar el máximo impacto empresarial, trabajando \n" +
+      "estrechamente con los miembros de mi equipo de DevOps para desarrollar las mejores soluciones escalables de su clase. Logramos reducir \n" +
+      "el tiempo de implementación de la infraestructura en un 150 por ciento, diseñando canales de CI/CD eficientes.\n" +
+      "En términos de mi conocimiento de herramientas y soluciones, soy competente en Datadog, Kubernetes, Azure, AWS y \n" +
+      "Nubes privadas junto con cinco lenguajes de programación principales.",
   },
   {
     id: 7,
@@ -74,8 +79,9 @@ export const speakers = [
     title: "Ingeniero Electrico",
     charla: "Innovación, el motor del desarrollo tecnológico",
     imageUrl: "public/speakers img/Juan González.jpeg",
-    borderColor: "#884E85",  // Color de borde personalizado
-    details: "Más de 15 años de experiencia en instalaciones eléctricas, diseño de sistemas de iluminación, asesor energético y consultor de transformación digital.\n"
+    borderColor: "#00cbfe", // Color de borde personalizado
+    details:
+      "Más de 15 años de experiencia en instalaciones eléctricas, diseño de sistemas de iluminación, asesor energético y consultor de transformación digital.\n",
   },
   {
     id: 8,
@@ -83,7 +89,7 @@ export const speakers = [
     title: "IT Consultant",
     charla: "Beyond Code: Integrated Security with DevSecOps and SSDLC",
     imageUrl: "public/speakers img/Karla Aquino.jpeg",
-    borderColor: "#026691",  // Color de borde personalizado
-    details: ""
+    borderColor: "#00cbfe", // Color de borde personalizado
+    details: "_________________________NO TIENE________________________",
   },
 ];
